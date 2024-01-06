574b4559d92f3b4d6af6c090e081935ff91ac7df
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-06 18:52:37 +0530

    style: loading background

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -2,7 +2,7 @@ import styles from "./index.module.css";
 
 const Loader = () => {
 	return (
-		<div className="bg-neutralBg w-screen h-screen flex justify-center items-center">
+		<div className="bg-neutralBg text-onNeutralBg w-screen h-screen flex justify-center items-center">
 			<div className={styles.loader}></div>
 		</div>
 	);
