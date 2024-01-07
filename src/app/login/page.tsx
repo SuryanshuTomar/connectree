33282d35b180b7893d9dce1733d557beb4dbbc0c
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-07 18:53:30 +0530

    add: loginpage structure

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,23 @@
+import SocialButton from "@/components/Buttons/SocialButton";
 import { FC } from "react";
 
 interface LoginPageProps {}
 
 const LoginPage: FC<LoginPageProps> = (props) => {
-	return <div>LoginPage</div>;
+	return (
+		<div>
+			<div className="mt-6 p-6 bg-onPrimaryBg text-primaryBg m-auto rounded max-w-sm border flex flex-col items-center">
+				<h1 className="my-4 mb-2 text-4xl font-bold text-center">
+					Sign In
+				</h1>
+
+				<p className="text-center mb-6">
+					Sign in to your account using one of the below sign-in options.
+				</p>
+				<SocialButton variant="google">Continue with Google</SocialButton>
+			</div>
+		</div>
+	);
 };
 
 export default LoginPage;
